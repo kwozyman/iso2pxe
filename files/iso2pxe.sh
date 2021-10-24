3f7c0c332861fa6ccf42d2e2b1de3f3e55ddd3d7
Costin Gament <cgament@redhat.com>
2021-10-25 01:18:49 +0200

    fix formatting

--- files/iso2pxe.sh
+++ files/iso2pxe.sh
@@ -17,15 +17,15 @@ cp /data/pxelinux.cfg ${tftp_path}/pxelinux.cfg/default
 for iso in $(find ${iso_paths} -name '*.iso'); do
   isoname=$(basename $iso | sed -e 's/.iso//g')
   mkdir -p ${tftp_path}/${isoname}
-    7z e ${iso} -o${tftp_path}/${isoname} images/pxeboot/vmlinuz
-    7z e ${iso} -o${tftp_path}/${isoname} images/pxeboot/initrd.img
-    7z e ${iso} -o${tftp_path}/${isoname} images/pxeboot/rootfs.img
-    7z e ${iso} -o${tftp_path}/${isoname} images/ignition.img
-    cat ${tftp_path}/${isoname}/ignition.img | gunzip | \
-      sed "s/.*config.ign.*/{/" | sed "s/.*TRAILER.*/}/" > ${tftp_path}/${isoname}/ignition.ign
-    echo "LABEL ${isoname}" >> ${tftp_path}/pxelinux.cfg/default
-    echo "  KERNEL ${isoname}/vmlinuz" >> ${tftp_path}/pxelinux.cfg/default
-    echo "  APPEND initrd=${isoname}/initrd.img,${isoname}/rootfs.img ignition.config.url=http://${webserver}/${isoname}/ignition.ign ignition.firstboot ignition.platform.id=metal" >> ${tftp_path}/pxelinux.cfg/default
+  7z e ${iso} -o${tftp_path}/${isoname} images/pxeboot/vmlinuz
+  7z e ${iso} -o${tftp_path}/${isoname} images/pxeboot/initrd.img
+  7z e ${iso} -o${tftp_path}/${isoname} images/pxeboot/rootfs.img
+  7z e ${iso} -o${tftp_path}/${isoname} images/ignition.img
+  cat ${tftp_path}/${isoname}/ignition.img | gunzip | \
+    sed "s/.*config.ign.*/{/" | sed "s/.*TRAILER.*/}/" > ${tftp_path}/${isoname}/ignition.ign
+  echo "LABEL ${isoname}" >> ${tftp_path}/pxelinux.cfg/default
+  echo "  KERNEL ${isoname}/vmlinuz" >> ${tftp_path}/pxelinux.cfg/default
+  echo "  APPEND initrd=${isoname}/initrd.img,${isoname}/rootfs.img ignition.config.url=http://${webserver}/${isoname}/ignition.ign ignition.firstboot ignition.platform.id=metal" >> ${tftp_path}/pxelinux.cfg/default
 done
 
 start_dhcp=$(echo ${hypervisor} | awk -F. '{print $1"."$2"."$3}').$(($(echo ${hypervisor} | awk -F. '{print $4}') + 1))
