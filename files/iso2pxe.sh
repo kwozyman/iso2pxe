b8c004d9917be6d34f414bddef50d97672737408
Javi Polo <javipolo@redhat.com>
2022-01-28 13:08:54 +0100

    Add iPXE support

--- files/iso2pxe.sh
+++ files/iso2pxe.sh
@@ -15,6 +15,7 @@ tftp_path=/tftpboot
 iso_paths=/data/iso
 cp /boot/efi/EFI/fedora/shimx64.efi /boot/efi/EFI/fedora/grubx64.efi ${tftp_path}/uefi
 chmod 777 ${tftp_path}/uefi/*
+cp /usr/share/ipxe/ipxe.lkrn ${tftp_path}
 cp /data/pxelinux.cfg ${tftp_path}/pxelinux.cfg/default
 cp /data/grub.cfg ${tftp_path}/uefi/grub.cfg
 for iso in $(find ${iso_paths} -name '*.iso'); do
@@ -27,10 +28,18 @@ for iso in $(find ${iso_paths} -name '*.iso'); do
   echo "LABEL ${isoname}" >> ${tftp_path}/pxelinux.cfg/default
   echo "  KERNEL ${isoname}/vmlinuz" >> ${tftp_path}/pxelinux.cfg/default
   echo "  APPEND initrd=${isoname}/initrd.img,${isoname}/rootfs.img ignition.config.url=http://${webserver}/${isoname}/config.ign ignition.firstboot ignition.platform.id=metal" >> ${tftp_path}/pxelinux.cfg/default
+  echo "LABEL ${isoname} iPXE" >> ${tftp_path}/pxelinux.cfg/default
+  echo "  KERNEL ipxe.lkrn" >> ${tftp_path}/pxelinux.cfg/default
+  echo "  APPEND dhcp && chain http://${webserver}/${isoname}/ipxe" >> ${tftp_path}/pxelinux.cfg/default
   echo "menuentry '${isoname}' {" >> ${tftp_path}/uefi/grub.cfg
   echo "  linux ${isoname}/vmlinuz coreos.live.rootfs_url=http://${webserver}/${isoname}/rootfs.img ignition.config.url=http://${webserver}/${isoname}/config.ign ignition.firstboot ignition.platform.id=metal" >> ${tftp_path}/uefi/grub.cfg
   echo "  initrd ${isoname}/initrd.img" >> ${tftp_path}/uefi/grub.cfg
   echo "}" >> ${tftp_path}/uefi/grub.cfg
+  echo '#!ipxe' >> ${tftp_path}/${isoname}/ipxe
+  echo "set web http://${webserver}/${isoname}"  >> ${tftp_path}/${isoname}/ipxe
+  echo 'kernel ${web}/vmlinuz coreos.live.rootfs_url=${web}/rootfs.img ignition.config.url=${web}/config.ign ignition.firstboot ignition.platform.id=metal'  >> ${tftp_path}/${isoname}/ipxe
+  echo 'initrd ${web}/initrd.img'  >> ${tftp_path}/${isoname}/ipxe
+  echo 'boot'  >> ${tftp_path}/${isoname}/ipxe
 done
 
 start_dhcp=$(echo ${hypervisor} | awk -F. '{print $1"."$2"."$3}').$(($(echo ${hypervisor} | awk -F. '{print $4}') + 1))
